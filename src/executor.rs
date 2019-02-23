055b421df421110d128621df106d2d922c29e880
Yusuf Simonson <simonson@gmail.com>
2019-02-23 17:08:14 -0500

    Split out execution-related functionality from main

---
+++ src/executor.rs
@@ -0,0 +1,117 @@
+use std::rc::Rc;
+
+use ketos::{Error, Value};
+use ketos::name::{is_system_fn, is_system_operator, standard_names};
+use ketos::compile::compile;
+use ketos::rc_vec::{RcString, RcVec};
+
+use crate::builtins;
+use crate::tui::{self, ArgsCompleter};
+
+pub fn exprs(interp: &builtins::Interpreter, exprs: &str, path: Option<String>) -> Result<Option<Value>, Error> {
+    let ketos_interp = interp.inner();
+    let mut values = ketos_interp.parse_exprs(exprs, path)?;
+
+    if values.len() == 0 {
+        return Ok(None);
+    }
+
+    // Automatically insert parens if they're not explicitly put
+    let input_value = match values.as_slice() {
+        [Value::List(_)] => values.pop().unwrap(),
+        _ => Value::List(RcVec::new(values))
+    };
+
+    let input_value = rewrite_exprs(interp, input_value);
+    let code = compile(ketos_interp.context(), &input_value)?;
+    let output_value = ketos_interp.execute_code(Rc::new(code))?;
+
+    // update the completions
+    tui::ARGS_COMPLETER.with(move |key| {
+        let mut completer = key.borrow_mut();
+        update_command_completions(interp, &mut completer, &input_value);
+    });
+
+    Ok(Some(output_value))
+}
+
+fn rewrite_exprs(interp: &builtins::Interpreter, value: Value) -> Value {
+    let ketos_interp = interp.inner();
+    let scope = ketos_interp.scope();
+
+    match value {
+        Value::List(list) => {
+            let list_v = list.into_vec();
+            let mut iter = list_v.into_iter();
+            let first_value = iter.next().unwrap();
+            let mut new_list = vec![];
+
+            if let Value::Name(first_name) = first_value {
+                if is_system_operator(first_name) {
+                    // These system operators need special handling because they
+                    // support lists where the first value can be a bare name
+
+                    new_list.push(Value::Name(first_name));
+
+                    match first_name {
+                        standard_names::LET | standard_names::DEFINE | standard_names::MACRO | standard_names::LAMBDA => {
+                            if let Some(first_arg) = iter.next() {
+                                new_list.push(first_arg);
+                            }
+                        }
+                        standard_names::STRUCT | standard_names::EXPORT | standard_names::USE => {
+                            while let Some(arg) = iter.next() {
+                                new_list.push(arg);
+                            }
+                        }
+                        _ => ()
+                    }
+                } else if !is_system_fn(first_name) && !scope.contains_name(first_name) {
+                    // Looks like this expr is shaped like a function call, to a
+                    // function that does not exist. Change this into a call to
+                    // `proc`.
+
+                    new_list.push(Value::Name(interp.proc_name));
+                    new_list.push({
+                        let name_store = scope.borrow_names();
+                        let first_name_str = name_store.get(first_name).to_string();
+                        Value::String(RcString::new(first_name_str.clone()))
+                    });
+                } else {
+                    new_list.push(first_value);
+                }
+            } else {
+                new_list.push(first_value);
+            }
+
+            new_list.extend(iter.map(|value| rewrite_exprs(interp, value)));
+            Value::List(RcVec::new(new_list))
+        }
+        Value::Name(name) if !is_system_fn(name) && !is_system_operator(name) && !scope.contains_name(name) => {
+            let name_store = scope.borrow_names();
+            let arg_str = name_store.get(name).to_string();
+            Value::String(RcString::new(arg_str))
+        }
+        _ => value
+    }
+}
+
+fn update_command_completions(interp: &builtins::Interpreter, completer: &mut ArgsCompleter, value: &Value) {
+    if let Value::List(list) = value {
+        let mut iter = list.iter();
+        let first_value = iter.next();
+        let second_value = iter.next();
+
+        match (first_value, second_value) {
+            (Some(Value::Name(first_name)), Some(Value::String(cmd))) if first_name == &interp.proc_name => {
+                // This seems to be a proc call - process the arguments
+                while let Some(value) = iter.next() {
+                    if let Value::String(arg) = value {
+                        completer.add(&cmd, &arg);
+                    }
+                }
+            },
+            _ => {}
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ extern crate nix;
 
 mod builtins;
 mod error;
+mod executor;
 mod util;
 mod tui;
 
@@ -20,17 +21,13 @@ use std::str::FromStr;
 use std::sync::Arc;
 use std::time::Duration;
 use std::fs::File;
-use std::rc::Rc;
 
 use gumdrop::{Options, ParsingStyle};
-use ketos::{Builder, Error, FromValueRef, RestrictConfig, Value};
-use ketos::name::{is_system_fn, is_system_operator, standard_names};
-use ketos::compile::compile;
+use ketos::{Builder, Error, FromValueRef, RestrictConfig};
 use ketos::io::{IoError, IoMode};
-use ketos::rc_vec::{RcString, RcVec};
 use linefeed::{Command, Interface, ReadResult, Signal};
 
-use crate::tui::{KnoshAccept, is_parseable, ArgsCompleter, KnoshCompleter};
+use crate::tui::{KnoshAccept, is_parseable, KnoshCompleter};
 
 const SOFT_MAX_PROMPT_LENGTH: u8 = 10;
 
@@ -120,7 +117,7 @@ fn run() -> i32 {
 
     if let Some(ref expr) = opts.expr {
         if !interactive {
-            match execute_exprs(&interp, &expr, None) {
+            match executor::exprs(&interp, &expr, None) {
                 Ok(Some(value)) => if let Ok(p) = <&builtins::ProcPromise>::from_value_ref(&value) {
                     if let Err(err) = p.run() {
                         display_error(&interp, "", &err);
@@ -214,114 +211,6 @@ fn display_error(interp: &builtins::Interpreter, prefix: &str, err: &Error) {
     eprintln!("{}{}", prefix, ketos_interp.format_error(err));
 }
 
-fn execute_exprs(interp: &builtins::Interpreter, exprs: &str, path: Option<String>) -> Result<Option<Value>, Error> {
-    let ketos_interp = interp.inner();
-    let mut values = ketos_interp.parse_exprs(exprs, path)?;
-
-    if values.len() == 0 {
-        return Ok(None);
-    }
-
-    // Automatically insert parens if they're not explicitly put
-    let input_value = match values.as_slice() {
-        [Value::List(_)] => values.pop().unwrap(),
-        _ => Value::List(RcVec::new(values))
-    };
-
-    let input_value = rewrite_commands(interp, input_value);
-    let code = compile(ketos_interp.context(), &input_value)?;
-    let output_value = ketos_interp.execute_code(Rc::new(code))?;
-
-    // update the completions
-    tui::ARGS_COMPLETER.with(move |key| {
-        let mut completer = key.borrow_mut();
-        update_command_completions(interp, &mut completer, &input_value);
-    });
-
-    Ok(Some(output_value))
-}
-
-fn rewrite_commands(interp: &builtins::Interpreter, value: Value) -> Value {
-    let ketos_interp = interp.inner();
-    let scope = ketos_interp.scope();
-
-    match value {
-        Value::List(list) => {
-            let list_v = list.into_vec();
-            let mut iter = list_v.into_iter();
-            let first_value = iter.next().unwrap();
-            let mut new_list = vec![];
-
-            if let Value::Name(first_name) = first_value {
-                if is_system_operator(first_name) {
-                    // These system operators need special handling because they
-                    // support lists where the first value can be a bare name
-
-                    new_list.push(Value::Name(first_name));
-
-                    match first_name {
-                        standard_names::LET | standard_names::DEFINE | standard_names::MACRO | standard_names::LAMBDA => {
-                            if let Some(first_arg) = iter.next() {
-                                new_list.push(first_arg);
-                            }
-                        }
-                        standard_names::STRUCT | standard_names::EXPORT | standard_names::USE => {
-                            while let Some(arg) = iter.next() {
-                                new_list.push(arg);
-                            }
-                        }
-                        _ => ()
-                    }
-                } else if !is_system_fn(first_name) && !scope.contains_name(first_name) {
-                    // Looks like this expr is shaped like a function call, to a
-                    // function that does not exist. Change this into a call to
-                    // `proc`.
-
-                    new_list.push(Value::Name(interp.proc_name));
-                    new_list.push({
-                        let name_store = scope.borrow_names();
-                        let first_name_str = name_store.get(first_name).to_string();
-                        Value::String(RcString::new(first_name_str.clone()))
-                    });
-                } else {
-                    new_list.push(first_value);
-                }
-            } else {
-                new_list.push(first_value);
-            }
-
-            new_list.extend(iter.map(|value| rewrite_commands(interp, value)));
-            Value::List(RcVec::new(new_list))
-        }
-        Value::Name(name) if !is_system_fn(name) && !is_system_operator(name) && !scope.contains_name(name) => {
-            let name_store = scope.borrow_names();
-            let arg_str = name_store.get(name).to_string();
-            Value::String(RcString::new(arg_str))
-        }
-        _ => value
-    }
-}
-
-fn update_command_completions(interp: &builtins::Interpreter, completer: &mut ArgsCompleter, value: &Value) {
-    if let Value::List(list) = value {
-        let mut iter = list.iter();
-        let first_value = iter.next();
-        let second_value = iter.next();
-
-        match (first_value, second_value) {
-            (Some(Value::Name(first_name)), Some(Value::String(cmd))) if first_name == &interp.proc_name => {
-                // This seems to be a proc call - process the arguments
-                while let Some(value) = iter.next() {
-                    if let Value::String(arg) = value {
-                        completer.add(&cmd, &arg);
-                    }
-                }
-            },
-            _ => {}
-        }
-    }
-}
-
 /// Executes a ketos file. This does not use ketos' built-in `run_file`
 /// because we do some pre-processing before shipping it off to the
 /// interpreter.
@@ -357,7 +246,7 @@ fn run_file(interp: &builtins::Interpreter, path: &Path) -> bool {
         if is_parseable(&buf) || i == line_count - 1 {
             let error_prefix = format!("line {}: ", i + 1);
 
-            match execute_exprs(interp, &buf, path_str.clone()) {
+            match executor::exprs(interp, &buf, path_str.clone()) {
                 Ok(Some(value)) => if let Ok(p) = <&builtins::ProcPromise>::from_value_ref(&value) {
                     if let Err(err) = p.run_suppressed() {
                         display_error(&interp, &error_prefix, &err);
@@ -418,7 +307,7 @@ fn run_repl(interp: &builtins::Interpreter) -> io::Result<()> {
                 interface.add_history(line.clone());
                 line.push('\n');
 
-                match execute_exprs(interp, &line, None) {
+                match executor::exprs(interp, &line, None) {
                     Ok(Some(value)) => if let Ok(p) = <&builtins::ProcPromise>::from_value_ref(&value) {
                         if let Err(err) = p.run() {
                             display_error(&interp, "", &err);
